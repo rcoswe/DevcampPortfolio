999de86c0d5e864772b28419666ddbfc43ced84c
Richard Cohen <rcoswemail@icloud.com>
2020-04-10 23:33:59 +0200

    Correction and clean-up of ApplicationController
    Sublime for some reason altered back to an earlier version. With the guest_user method directly in this file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
-
-  def current_user
-    super || guest = OpenStruct.new(name: "Guest User", 
-                                    first_name: "Guest", 
-                                    email: "guest@example.com"
-                                    )
-  end
+  include CurrentUserConcern
 end
